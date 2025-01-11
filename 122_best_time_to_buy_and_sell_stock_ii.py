efb1e8aa63bc9d152b9b4ab0b73d264a624b271d
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-01-11 02:00:54 -0600

    Update 122_best_time_to_buy_and_sell_stock_ii.py - LeetCode Problem 122

--- 122_best_time_to_buy_and_sell_stock_ii.py
+++ 122_best_time_to_buy_and_sell_stock_ii.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # JUST BEST PRICE TO BUY AND SELL STOCKS I
+        # JUST BEST PRICE TO BUY AND SELL STOCKS I WE PREVIOUSLY DID
         res = 0
-        # SUMMING EVERYTHING UPWARDS AND CHECKING BACK 
+        # SUMMING EVERYTHING UPWARDS AND CHECKING BACK
         for r in range(1, len(prices)):
-            if prices[r] > prices[r-1]:
-                res += prices[r] - prices[r-1]
+            if prices[r] > prices[r - 1]:
+                res += prices[r] - prices[r - 1]
         return res
