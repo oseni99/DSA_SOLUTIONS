6d1d2c9d3ed72570ee16e175984ef4cf471d72d2
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-01-11 00:54:24 -0600

    Add 122_best_time_to_buy_and_sell_stock_ii.py - LeetCode Problem 122

---
+++ 122_best_time_to_buy_and_sell_stock_ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # JUST BEST PRICE TO BUY AND SELL STOCKS I
+        res = 0
+        # SUMMING EVERYTHING UPWARDS AND CHECKING BACK 
+        for r in range(1, len(prices)):
+            if prices[r] > prices[r-1]:
+                res += prices[r] - prices[r-1]
+        return res
