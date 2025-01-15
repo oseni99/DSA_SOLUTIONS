11eac7609cdd9337b3391eb25fb8e5ff7f714097
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-01-15 13:30:15 -0600

    Update 88_merge_sorted_array.py - LeetCode Problem 88

--- 88_merge_sorted_array.py
+++ 88_merge_sorted_array.py
@@ -3,27 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        #  let me say
-        nums1[m:] = nums2
-
-        def quicksort(arr):
-            # do quick sort on this
-            def partition(low, high):
-                i = low - 1
-                pivot = arr[high]
-                for j in range(low, high):
-                    if arr[j] <= arr[high]:
-                        i += 1
-                        arr[i], arr[j] = arr[j], arr[i]
-                arr[i + 1], arr[high] = arr[high], arr[i + 1]
-                return i + 1
-
-            def quick_sort_recursive(low, high):
-                if low < high:
-                    piv_idx = partition(low, high)
-                    quick_sort_recursive(low, piv_idx - 1)
-                    quick_sort_recursive(piv_idx + 1, high)
-
-            quick_sort_recursive(0, len(arr) - 1)
-
-        quicksort(nums1)
+        a = m - 1
+        b = n - 1
+        i = len(nums1) - 1
+        # i want to start from the back
+        #  check if that number before 0 is less than or greater than
+        # since its sorted in increasimg order
+        #  that i is what we use to insert it
+        # TC of O(m+n)
+        while a >= 0 and b >= 0:
+            if nums1[a] <= nums2[b]:
+                nums1[i] = nums2[b]
+                b -= 1
+            else:
+                nums1[i] = nums1[a]
+                a -= 1
+            i -= 1
+        #  case where i still have elements left inside the nums2
+        while b >= 0:
+            nums1[i] = nums2[b]
+            b -= 1
+            i -= 1
