4e659d569df01e136a4207568e2105e022d122cc
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-01-15 23:13:41 -0600

    Update 15_3sum.py - LeetCode Problem 15

--- 15_3sum.py
+++ 15_3sum.py
@@ -1,29 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+        # i pick one number and i do the BS on it to see if it matches
         res = []
-
+        #  i have to also sort it for BS to work
+        # i have to skip it also if the current number is the same as the previous
+        # because we will have the same answer again which is duplictes
+        nums.sort()
         for i in range(len(nums)):
-            # Skip duplicate numbers for the first element
             if i > 0 and nums[i] == nums[i - 1]:
-                continue
-
-            l, r = i + 1, len(nums) - 1  # Two pointers, left and right
-
+                continue  #  skip it
+            l = i + 1
+            r = len(nums) - 1
             while l < r:
-                three_sum = nums[i] + nums[l] + nums[r]
-                if three_sum > 0:
-                    r -= 1
-                elif three_sum < 0:
-                    l += 1
-                else:
+                target = nums[i] + nums[l] + nums[r]
+                if target == 0:
                     res.append([nums[i], nums[l], nums[r]])
                     l += 1
                     r -= 1
-                    # Skip duplicates for l
+                    # there might be multiple results using that same i
                     while l < r and nums[l] == nums[l - 1]:
                         l += 1
-                    # Skip duplicates for r
                     while l < r and nums[r] == nums[r + 1]:
                         r -= 1
+                if target > 0:
+                    r -= 1
+                if target < 0:
+                    l += 1
         return res
