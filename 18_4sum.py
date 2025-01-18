d65aeb2f4d616c473f22c3321d8f2ccc3a6d80e7
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-01-17 23:37:35 -0600

    Update 18_4sum.py - LeetCode Problem 18

--- 18_4sum.py
+++ 18_4sum.py
@@ -1,2 +1,39 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # i can now take a number and do 3sum on it
+        #  now add everything inside the result
+        new_nums = sorted(nums)
+        def three_sum(arr, new_target):
+            three_sum_res = []
+            for i in range(len(arr)):
+                #  checking for unique should be here i +1 and i
+                if i > 0 and arr[i] == arr[i - 1]:
+                    continue
+                l = i + 1
+                r = len(arr) - 1
+                while l < r:
+                    total = arr[i] + arr[l] + arr[r]
+                    if total == new_target:
+                        three_sum_res.append([arr[i], arr[l], arr[r]])
+                        l += 1
+                        r -= 1
+                        while l < r and arr[l] == arr[l - 1]:
+                            l += 1
+                        while l < r and arr[r] == arr[r + 1]:
+                            r -= 1
+                    elif total < target:
+                        l += 1
+                    else:
+                        r -= 1
+            return three_sum_res
+
+        total_res = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            a = three_sum(nums[i + 1 :], target - nums[i])
+            for s in a:
+                if len(s) > 0:
+                    s.append(nums[i])
+                    total_res.append(s)
+        return total_res
