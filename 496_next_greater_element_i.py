e65f24ac0a79836dcd5d69936fa01cf514933111
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-01-30 13:08:24 -0600

    Update 496_next_greater_element_i.py - LeetCode Problem 496

--- 496_next_greater_element_i.py
+++ 496_next_greater_element_i.py
@@ -2,16 +2,15 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         # i have to use a stack
         # monotonic stack and i use it to find whats greater each time
-        res = [-1] * len(nums2)
+        # i can also create a hashmap that holds number and its greater
+        res = {}
         mono_stack = []
-        for i, j in enumerate(nums2):
-            while mono_stack and nums2[mono_stack[-1]] < j:
+        for num in nums2:
+            while mono_stack and mono_stack[-1] < num:
                 idx = mono_stack.pop()
-                res[idx] = j
-            mono_stack.append(i)
-        total_res = [-1] * len(nums1)
-        for i, j in enumerate(nums1):
-            if j in nums2:
-                idx = nums2.index(j)
-                total_res[i] = res[idx]
-        return total_res
+                res[idx] = num
+            mono_stack.append(num)
+        # this will also have some elements left so i just change that to -1 cuz no nex greater
+        for i in mono_stack:
+            res[i] = -1
+        return [res[i] for i in nums1]
