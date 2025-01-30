7d18b131452b3ceb7f65c17f1d0fca4368c4cebf
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-01-30 17:47:07 -0600

    Update 88_merge_sorted_array.py - LeetCode Problem 88

--- 88_merge_sorted_array.py
+++ 88_merge_sorted_array.py
@@ -3,24 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        a = m - 1
-        b = n - 1
+        l = m - 1
+        r = n - 1
         i = len(nums1) - 1
-        # i want to start from the back
-        #  check if that number before 0 is less than or greater than
-        # since its sorted in increasimg order
-        #  that i is what we use to insert it
-        # TC of O(m+n)
-        while a >= 0 and b >= 0:
-            if nums1[a] <= nums2[b]:
-                nums1[i] = nums2[b]
-                b -= 1
+        while r >= 0 and l >= 0:
+            if nums1[l] < nums2[r]:
+                nums1[i] = nums2[r]
+                r -= 1
             else:
-                nums1[i] = nums1[a]
-                a -= 1
+                nums1[i] = nums1[l]
+                l -= 1
             i -= 1
-        #  case where i still have elements left inside the nums2
-        while b >= 0:
-            nums1[i] = nums2[b]
-            b -= 1
+        # this handles lets say every element is bigger than the r e.g [4,5,6] [1,2,3]
+        while r >= 0:
+            nums1[i] = nums2[r]
+            r -= 1
             i -= 1
