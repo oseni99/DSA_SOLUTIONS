f4d1babe02b20c2b23adb34614f738a0e1cb7123
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-02-02 00:11:12 -0600

    Update 567_permutation_in_string.py - LeetCode Problem 567

--- 567_permutation_in_string.py
+++ 567_permutation_in_string.py
@@ -1,24 +1,23 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-        # basically if i have a permutation i can just throw it inside that hashmap and start dynamicaly checking it 
-        a_hash = Counter(s1)
-        b_hash = {}
+        # throw s1 first into a hashmap
+        first = Counter(s1)
+        second = {}
+        n = len(s1)
         l = 0
         for r in range(len(s2)):
-            if s2[r] in b_hash:
-                b_hash[s2[r]] += 1
+            if s2[r] in second:
+                second[s2[r]] += 1
             else:
-                b_hash[s2[r]] = 1
+                second[s2[r]] = 1
 
-            if r - l + 1 > len(s1):
-                if b_hash[s2[l]] == 1:
-                    del b_hash[s2[l]]
+            if r - l + 1 == n:
+                if first == second:
+                    return True
                 else:
-                    b_hash[s2[l]] -= 1
+                    if second[s2[l]] == 1:
+                        del second[s2[l]]
+                    else:
+                        second[s2[l]] -= 1
                 l += 1
-
-            if a_hash == b_hash:
-                return True
         return False
