1a72e4127f25744e23f8ccf8c5a6eb7b721d45ff
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-02-03 01:00:08 -0600

    Add 232_implement_queue_using_stacks.py - LeetCode Problem 232

---
+++ 232_implement_queue_using_stacks.py
@@ -0,0 +1,31 @@
+class MyQueue:
+
+    def __init__(self):
+        self.push_stack = []
+        self.pop_stack = []
+
+    def push(self, x: int) -> None:
+        self.push_stack.append(x)
+
+    def pop(self) -> int:
+        if not self.pop_stack:
+            while self.push_stack:
+                self.pop_stack.append(self.push_stack.pop())
+        return self.pop_stack.pop()
+
+    def peek(self) -> int:
+        if not self.pop_stack:
+            while self.push_stack:
+                self.pop_stack.append(self.push_stack.pop())
+        return self.pop_stack[-1]
+
+    def empty(self) -> bool:
+        return len(self.push_stack) == 0 and len(self.pop_stack) == 0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
