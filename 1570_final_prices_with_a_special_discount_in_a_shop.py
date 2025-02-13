d4d554efff89b93b019537f128215932e58ddbf2
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-02-12 22:50:19 -0600

    Add 1570_final_prices_with_a_special_discount_in_a_shop.py - LeetCode Problem 1570

---
+++ 1570_final_prices_with_a_special_discount_in_a_shop.py
@@ -0,0 +1,11 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        # approach with like next smaller then reduce 
+        stack = []
+        res = prices
+        for i in range(len(prices)):
+            while stack and prices[stack[-1]] >= prices[i]:
+                idx = stack.pop()
+                res[idx] = prices[idx] - prices[i]
+            stack.append(i)
+        return res
