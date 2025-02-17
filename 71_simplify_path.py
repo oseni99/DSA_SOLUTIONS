19fea5df2b18803cc75e690ff1ea13c30ecb2e7b
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-02-17 01:00:31 -0600

    Update 71_simplify_path.py - LeetCode Problem 71

--- 71_simplify_path.py
+++ 71_simplify_path.py
@@ -2,26 +2,12 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         res = path.split("/")
         stack = []
-        ans = []
         for i in res:
-            if i == "...":
-                stack.append("...")
-            elif i == "..":
-                stack.pop()
-            elif i == "":
-                stack.append("/")
-            elif i == ".":
+            if i == "..":
+                if stack:
+                    stack.pop()
+            elif i == "." or i == "":
                 continue
             else:
                 stack.append(i)
-        print(stack)
-        while len(stack) > 1 and stack[-1] == "/":
-            stack.pop()
-        for i in range(len(stack)):
-            if stack[i] != "/" and i < len(stack) - 1:
-                a = stack[i] + "/"
-                ans.append(a)
-            else:
-                ans.append(stack[i])
-        print(ans)
-        return "".join(ans)
+        return "/" + "/".join(stack)
