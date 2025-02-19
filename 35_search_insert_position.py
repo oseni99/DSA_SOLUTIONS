b1572f625a34b1b63045dcbcdbf9edbde3b187b1
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-02-19 00:24:51 -0600

    Update 35_search_insert_position.py - LeetCode Problem 35

--- 35_search_insert_position.py
+++ 35_search_insert_position.py
@@ -1,14 +1,18 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        # use binary search
+        #  [1,3,5,6]
+        #  whenever i cant just return that middle index plus one
         l = 0
         r = len(nums) - 1
         while l <= r:
             m = (l + r) // 2
-            if target > nums[m]:
-                l = m + 1
-            elif target < nums[m]:
-                r = m - 1
-            else:
+            if nums[m] == target:
                 return m
-        return l
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        if nums[m] < target:
+            return m + 1
+        else:
+            return 0
