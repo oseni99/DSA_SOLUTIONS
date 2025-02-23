4ce6758d2991eb2bbda91c1c58f9a0fb45cb1683
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-02-23 02:24:40 -0600

    Update 33_search_in_rotated_sorted_array.py - LeetCode Problem 33

--- 33_search_in_rotated_sorted_array.py
+++ 33_search_in_rotated_sorted_array.py
@@ -2,7 +2,7 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # first thing i should do is find that position where it changes
         # because if its sorted the position where it changes from high to low is where it changes
-        # where it goes from the largest to smallest is where it rotates 
+        # where it goes from the largest to smallest is where it rotates
 
         def find_pivot(arr):
             l = 0
@@ -17,11 +17,9 @@ class Solution:
 
         # after seeing where it changes
         # if pivot is greater than number then its left side else right side
-        # this allows us to know where to run the binary search in 
-        
-        def binary_search(arr):
-            l = 0
-            r = len(arr) - 1
+        # this allows us to know where to run the binary search in
+
+        def binary_search(arr, l, r):
             while l <= r:
                 m = (l + r) // 2
                 if nums[m] == target:
@@ -33,9 +31,15 @@ class Solution:
             return -1
 
         pivot_idx = find_pivot(nums)
+        l = 0
+        r = len(nums) - 1
+        # i know that if i my smallest is smaller than right starughtaway then its not rotated
+        if nums[l] < nums[r]:
+            return binary_search(nums, l, r)
+
         if nums[pivot_idx] == target:
             return pivot_idx
         elif nums[pivot_idx] < target:
-            return binary_search(nums[:pivot_idx])
+            return binary_search(nums, l=0, r=pivot_idx - 1)
         else:
-            return binary_search(nums[pivot_idx:])
+            return binary_search(nums, l=pivot_idx, r=len(nums))
