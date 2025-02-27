2827a5b97392732924dc28c302d8021fd229581a
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-02-27 00:01:38 -0600

    Update 81_search_in_rotated_sorted_array_ii.py - LeetCode Problem 81

--- 81_search_in_rotated_sorted_array_ii.py
+++ 81_search_in_rotated_sorted_array_ii.py
@@ -1,38 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        # it is similar but i think it
-        myarr = list(set(nums))
-        print(myarr)
-
-        def find_pivot(myarr):
-            l = 0
-            r = len(myarr) - 1
-            while l < r:
-                m = (l + r) // 2
-                if myarr[m] > myarr[r]:
-                    l = m + 1
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] == target:
+                return True
+            if nums[l] < nums[m]:  # left portion
+                if nums[l] <= target < nums[m]:  # checking if its in left sorted part
+                    r = m - 1
                 else:
-                    r = m
-            return l
-
-        def binary_search(l, r):
-            while l <= r:
-                m = (l + r) // 2
-                if myarr[m] == target:
-                    return True
-                elif myarr[m] < target:
+                    l = m + 1
+            elif nums[l] > nums[m]:  # right portion
+                if ( nums[m] < target <= nums[r]):  # checking if target is in right sorted part
                     l = m + 1
                 else:
                     r = m - 1
-            return False
-
-        pivot_idx = find_pivot(myarr)
-        l = 0
-        r = len(nums) - 1
-
-        # if nums[l] <= nums[r]:
-        #     return binary_search(l, r)
-        # what i now use to check where to do the bs is if its greater or equal to the first one or it isnt
-        return binary_search(l=0, r=pivot_idx - 1) or binary_search(
-            l=pivot_idx, r=len(myarr) - 1
-        )
+            else:  # if its equal i just want to shrink it and move l by 1
+                l += 1
+        return False
