99ca0cddf74ff9e7eb7b541cf3b62b422c8ffb49
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-02-27 01:40:53 -0600

    Update 1023_time_based_key_value_store.py - LeetCode Problem 1023

--- 1023_time_based_key_value_store.py
+++ 1023_time_based_key_value_store.py
@@ -1,37 +1,32 @@
 class TimeMap:
 
     def __init__(self):
-        # create a dictionary based on value with key 
+        # create a dictionary based on value with key
         self.store = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key in self.store:
-            self.store[key].append([value,timestamp])
+            self.store[key].append([value, timestamp])
         else:
-            self.store[key] = [[value,timestamp]]
+            self.store[key] = [[value, timestamp]]
 
     def get(self, key: str, timestamp: int) -> str:
-        # let me do a bs on it 
-        arr = []
-        if key in self.store:
-            arr.extend(self.store[key])
-        else:
+        # let me do a bs on it
+        if key not in self.store:
             return ""
-        l = 0 
-        r = len(arr) - 1 
+        arr = self.store[key]
+        l, r = 0, len(arr) - 1
         while l <= r:
-            m = l + (r-l) // 2 
+            m = l + (r - l) // 2
             if arr[m][1] == timestamp:
                 return arr[m][0]
             elif arr[m][1] > timestamp:
-                r = m - 1 
+                r = m - 1
             else:
-                l = m + 1 
-        # i know that lowest will be on that left so highest should be on the right in bs 
-        return arr[r][0]
-
-    
-        
+                l = m + 1
+        # i know that lowest will be on that left so highest should be on the right in bs
+        # one bug is that what if r is -1 which means that it doesnt become valid
+        return arr[r][0] if r >= 0 else ""
 
 
 # Your TimeMap object will be instantiated and called as such:
