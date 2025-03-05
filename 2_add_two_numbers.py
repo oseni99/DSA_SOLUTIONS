1812d36b5f547332b166a9e6a3f469ba5073f864
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-03-05 01:02:02 -0600

    Add 2_add_two_numbers.py - LeetCode Problem 2

---
+++ 2_add_two_numbers.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        # create a new listNode to store the sum of those numbers
+        # so two pointers to check
+        dummy = ListNode(0)
+        curr = dummy
+        overs = None
+        while l1 and l2:
+            res = l1.val + l2.val
+            if overs:
+                res += overs
+            if res < 10:
+                curr.next = ListNode(res)
+            else:
+                new_res = str(res)
+                overs = 1
+                curr.next = ListNode(int(new_res[-1]))
+            curr = curr.next
+            l1 = l1.next
+            l2 = l2.next
+        # i know that if one is shorter than the other
+        while l1:
+            res = l1.val
+            if overs:
+                res += overs
+            if res < 10:
+                curr.next = ListNode(res)
+            else:
+                new_res = str(res)
+                overs = 1
+                curr.next = ListNode(int(new_res[-1]))
+            l1 = l1.next
+            curr = curr.next
+        while l2:
+            res = l2.val
+            if overs:
+                res += overs
+            if res < 10:
+                curr.next = ListNode(res)
+            else:
+                new_res = str(res)
+                overs = 1
+                curr.next = ListNode(int(new_res[-1]))
+            l2 = l2.next
+            curr = curr.next
+        return dummy.next
