7b7b03ea4ad0ad07e1068646878c0225e3a7e990
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-03-25 02:34:04 -0500

    Update 145_binary_tree_postorder_traversal.py - LeetCode Problem 145

--- 145_binary_tree_postorder_traversal.py
+++ 145_binary_tree_postorder_traversal.py
@@ -6,15 +6,19 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # post order is from left to root
-        self.res = []
-
-        def postorder(root):
-            if not root:
-                return
-            postorder(root.left)
-            postorder(root.right)
-            self.res.append(root.val)
-
-        postorder(root)
-        return self.res
+        # for the post order it moves from left to right to root
+        # we use two stack and use the dfs for the root left right
+        if not root:
+            return []
+        stack_1 = [root]  # the dfs
+        stack_2 = []  # this is what i use to reverse it
+        curr = root
+        while stack_1:
+            curr = stack_1.pop()
+            stack_2.append(curr.val)
+            if curr.left:
+                stack_1.append(curr.left)
+            if curr.right:
+                stack_1.append(curr.right)
+        res = [i for i in reversed(stack_2)]
+        return res
