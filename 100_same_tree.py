5b4865c1aaa7dafc9af435a0f6d706164ef4d980
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-03-26 02:10:26 -0500

    Update 100_same_tree.py - LeetCode Problem 100

--- 100_same_tree.py
+++ 100_same_tree.py
@@ -6,10 +6,13 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # this is when both doesnt exists
         if not p and not q:
             return True
+        # if one of them doesnt exist
         if not p or not q:
             return False
+        # if both exists now i check the values
         if p.val != q.val:
             return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
