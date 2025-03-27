2436ce56d5f8f7034446c3e6bf6747958d9ff9e8
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-03-27 09:10:46 -0500

    Add 784_insert_into_a_binary_search_tree.py - LeetCode Problem 784

---
+++ 784_insert_into_a_binary_search_tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # since its a bst its easier and you only check one side of the tree
+        # once you now get to the leaf node you add to the valid side
+        if root is None:
+            return TreeNode(val)
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
