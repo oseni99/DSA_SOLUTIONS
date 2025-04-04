dee7438b05fd2f5e93d10911c3fe44a20598598a
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-04-04 03:18:17 -0500

    Add 1013_fibonacci_number.py - LeetCode Problem 1013

---
+++ 1013_fibonacci_number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        # just used dp bottm to top approach 
+        dp = [0, 1]
+        if n < 2:
+            return n
+        i = 2
+        while i <= n:
+            dp[1], dp[0] = dp[1] + dp[0], dp[1]
+            i += 1
+        return dp[1]
