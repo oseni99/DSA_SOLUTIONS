ed43e0a302142f6a8c3e44b9a850ccabdd10ca80
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-04-12 05:58:37 -0500

    Update 1993_sum_of_all_subset_xor_totals.py - LeetCode Problem 1993

--- 1993_sum_of_all_subset_xor_totals.py
+++ 1993_sum_of_all_subset_xor_totals.py
@@ -1,13 +1,15 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        # subarray is diff from subset and the XOR is just dealing with binary
-        # we keep a track of all the total and its 2 to the power of n
+        #  i now understand what the xor means and it just means that when we get the subset we do the xor and we sum it together
+        # now in python to get the xor we just have to do the num ^ the other number
+        # it makes sense to now do it in this sense that as im going through the recursion stack/backtracking
+        # im just adding the xor together so its pretty easy for me
 
         def dfs(i, total):
             # base case
             if i == len(nums):
                 return total
-            #  two things can happen
+
             return dfs(i + 1, total ^ nums[i]) + dfs(i + 1, total)
 
         return dfs(0, 0)
