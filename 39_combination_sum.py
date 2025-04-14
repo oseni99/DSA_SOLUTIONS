44dc35d60a14d1a4ea2be4c575b7cdd167aa0859
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-04-13 21:18:19 -0500

    Update 39_combination_sum.py - LeetCode Problem 39

--- 39_combination_sum.py
+++ 39_combination_sum.py
@@ -1,22 +1,22 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
+        # im using a number multiple times also to check
+        # dfs with backtracking
 
-        def dfs(idx, subsets, curr_sum):
+        def dfs(i, path, curr_sum):
             if curr_sum == target:
-                res.append(subsets.copy())
+                res.append(path.copy())
                 return
-
             if curr_sum > target:
                 return
 
-            for i in range(idx, len(candidates)):
-                # i want to add it
-                subsets.append(candidates[i])
-                # because i can use it again i dont have to increase the i
-                dfs(i, subsets, curr_sum + candidates[i])
-                # i also want to backtrack if im done
-                subsets.pop()
+            for j in range(i, len(candidates)):
+                path.append(candidates[j])
+                curr_sum += candidates[j]
+                dfs(j, path, curr_sum)
+                path.pop()
+                curr_sum -= candidates[j]
 
         dfs(0, [], 0)
         return res
