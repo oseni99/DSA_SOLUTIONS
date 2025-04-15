11516810b7ddf8fd941651c2d4c0629a2fd3dc5c
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-04-15 03:34:03 -0500

    Update 47_permutations_ii.py - LeetCode Problem 47

--- 47_permutations_ii.py
+++ 47_permutations_ii.py
@@ -1,32 +1,21 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        res = []
-        used = [False] * len(
-            nums
-        )  # to know what number ive used in the recursion stack
 
-        # its basically me just getting the whole combinations thats there
-        # because it has to be unique im going to keep track of the
+        res = []
+        nums.sort()
+        # i just have to check if ive used that index where that number is
+        used = [False] * len(nums)
 
         def dfs(path):
             if len(path) == len(nums):
                 res.append(path.copy())
                 return
-
             for i in range(len(nums)):
-                # if ive used the number before
-                if used[i]:
+                if used[i] or (i > 0 and nums[i] == nums[i - 1] and not used[i - 1]):
                     continue
-
-                # i also want to skip duplicates
-                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
-                    continue
-
-                used[i] = True
                 path.append(nums[i])
+                used[i] = True
                 dfs(path)
-
                 path.pop()
                 used[i] = False
 
