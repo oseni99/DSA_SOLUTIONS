16a5a31a5d5797a63936bb62236638e3632b0d8a
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-04-15 02:40:54 -0500

    Update 90_subsets_ii.py - LeetCode Problem 90

--- 90_subsets_ii.py
+++ 90_subsets_ii.py
@@ -1,15 +1,16 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        # each number used only once
         res = []
         nums.sort()
 
-        def dfs(idx, path):
+        def dfs(i, path):
             res.append(path.copy())
-            for i in range(idx, len(nums)):
-                if i > idx and nums[i] == nums[i - 1]:
+            for j in range(i, len(nums)):
+                if j > i and nums[j] == nums[j - 1]:
                     continue
-                path.append(nums[i])
-                dfs(i + 1, path)
+                path.append(nums[j])
+                dfs(j + 1, path)
                 path.pop()
 
         dfs(0, [])
