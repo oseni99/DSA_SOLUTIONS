fe1acaec977a590daa761cfa7e1031d29d3d63cd
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-04-17 16:35:34 -0500

    Update 17_letter_combinations_of_a_phone_number.py - LeetCode Problem 17

--- 17_letter_combinations_of_a_phone_number.py
+++ 17_letter_combinations_of_a_phone_number.py
@@ -1,6 +1,5 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        # if i pick any number i have to find the hashmap and combine every letter
         store = {
             "2": "abc",
             "3": "def",
@@ -11,18 +10,20 @@ class Solution:
             "8": "tuv",
             "9": "wxyz",
         }
+
         res = []
+
         if not digits:
             return []
 
-        def dfs(idx, curr_str):
+        def dfs(i, path):
             # base case
-            if len(digits) == len(curr_str):
-                res.append(curr_str)
+            if len(path) == len(digits):
+                res.append(path)
                 return
-            letters = store[digits[idx]]
-            for i in letters:
-                dfs(idx + 1, curr_str + i)
+            letters = store[digits[i]]
+            for j in letters:
+                dfs(i + 1, path + j)
 
         dfs(0, "")
         return res
