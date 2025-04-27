a79a107b62496ba6e74f03cc34a46392bcb74b80
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-04-26 23:40:13 -0500

    Update 138_copy_list_with_random_pointer.py - LeetCode Problem 138

--- 138_copy_list_with_random_pointer.py
+++ 138_copy_list_with_random_pointer.py
@@ -7,33 +7,27 @@ class Node:
         self.random = random
 """
 
-class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        # its just like creating a clone of those nodes 
-        # add it like an overhead where 7--> 7 ---> 13 ---> 13 ---> 11 etc 
-        # after that i now link that random to it 
-        curr = head 
-        while curr:
-            temp = curr.next 
-            new_node = Node(curr.val) 
-            curr.next = new_node 
-            new_node.next = temp 
-            curr = temp 
 
-        # random part 
-        curr = head 
-        while curr:
-            if curr.random:
-                curr.next.random = curr.random.next
-            curr = curr.next.next 
-        
-        # detach what i added 
-        dummy = Node(0)
+class Solution:
+    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
+        # if no node
+        if not head:
+            return None
+
+        hash_store = {}
         curr = head
-        new_curr = dummy
+        # add the clone to the hash in one pass
         while curr:
-            new_curr.next = curr.next 
-            new_curr = new_curr.next 
-            curr.next = curr.next.next
-            curr = curr.next 
-        return dummy.next
+            clone = Node(curr.val)
+            hash_store[curr] = clone
+            curr = curr.next
+
+        # now add the next and randoms
+        curr = head
+        while curr:
+            old = hash_store[curr]
+            old.next = hash_store.get(curr.next)
+            old.random = hash_store.get(curr.random)
+            curr = curr.next
+
+        return hash_store[head]
