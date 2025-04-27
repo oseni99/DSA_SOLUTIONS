ac92684a3f02f60464a22f742bae41f90496bc7f
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-04-27 16:03:02 -0500

    Update 2_add_two_numbers.py - LeetCode Problem 2

--- 2_add_two_numbers.py
+++ 2_add_two_numbers.py
@@ -7,38 +7,30 @@ class Solution:
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        # create a new listNode to store the sum of those numbers
-        # so two pointers to check
-        dummy = ListNode(0)
-        curr = dummy
-        overs = 0
+        # this is just a case of where i know its a single digit so
+        # i think if i do a modulus of 10 and i get the remainder then i take the value and remainder
+        res = ListNode(0)
+        rem = 0
+        node = res
         while l1 and l2:
-            res = l1.val + l2.val
-            if overs:
-                res += overs
-            overs = res // 10
-            curr.next = ListNode(res % 10)
-            curr = curr.next
+            curr = rem + l1.val + l2.val
+            node.next = ListNode((curr % 10))
+            rem = curr // 10
             l1 = l1.next
             l2 = l2.next
-        # i know that if one is shorter than the other
+            node = node.next
         while l1:
-            res = l1.val
-            if overs:
-                res += overs
-            overs = res // 10
-            curr.next = ListNode(res % 10)
+            curr = rem + l1.val
+            node.next = ListNode((curr % 10))
+            rem = curr // 10
             l1 = l1.next
-            curr = curr.next
+            node = node.next
         while l2:
-            res = l2.val
-            if overs:
-                res += overs
-            overs = res // 10
-            curr.next = ListNode(res % 10)
+            curr = rem + l2.val
+            node.next = ListNode((curr % 10))
+            rem = curr // 10
             l2 = l2.next
-            curr = curr.next
-        # this will check if theres any overs added left 
-        if overs:
-            curr.next = ListNode(overs)
-        return dummy.next
+            node = node.next
+        if rem != 0:
+            node.next = ListNode(rem)
+        return res.next
