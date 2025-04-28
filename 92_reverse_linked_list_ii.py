0a7eaee58133bcbf3d82fb1c14d0f7e8c2fb7a07
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-04-28 02:39:05 -0500

    Update 92_reverse_linked_list_ii.py - LeetCode Problem 92

--- 92_reverse_linked_list_ii.py
+++ 92_reverse_linked_list_ii.py
@@ -5,20 +5,31 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        dummy = ListNode(0,head)
-        prev = dummy 
-        #  get left - 1 to know the prev 
-        for _ in range(left-1):
-            prev = prev.next 
+        # very similar to normal reverse a linked list
+        # the left anf right is places and its 1 indexed
+        # do a normal reversal but use it with a for loop
 
-        # i want to be able to reverse from left to rifht 
-        curr = prev.next 
-        prev_reversed = None 
+        # a dummyNode for if left is 1
+
+        dummy = ListNode(0, head)
+        curr = dummy
+
+        for _ in range(left - 1):
+            curr = curr.next
+
+        prevLeft = curr
+        curr = curr.next
+
+        prev = None
+        first = curr
         for _ in range(right - left + 1):
-            temp = curr.next 
-            curr.next = prev_reversed 
-            prev_reversed = curr 
-            curr = temp 
-        prev.next.next = curr 
-        prev.next = prev_reversed 
+            # now reverse
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        prevLeft.next = prev
+        first.next = curr
+
         return dummy.next
