e654fe54999f5f33eb1da5ae513d79786c6c5274
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-04-30 04:23:16 -0500

    Update 473_matchsticks_to_square.py - LeetCode Problem 473

--- 473_matchsticks_to_square.py
+++ 473_matchsticks_to_square.py
@@ -1,25 +1,27 @@
 class Solution:
     def makesquare(self, matchsticks: List[int]) -> bool:
-        # we know that a square has 4 sides and each are all symmetrical
-        # we have to divide the sum of the length by 4 first to get the sides
-        # it has to be an integer and not a float
-        total = sum(matchsticks)
-        if total % 4 != 0:
+        # first of all i know that a square has 4 equal sides
+        # if its sum isnt divisible by 4 it cant ever turn the matchsticks
+
+        total_sticks = sum(matchsticks)
+        if total_sticks % 4 != 0:
             return False
 
-        length = total // 4
-        sides = [0] * 4  # building the sides of the square
-        matchsticks.sort(
-            reverse=True
-        )  # if the biggest is less than length then it wont work
+        # moving to the next
+        # i know that each side has to be at least total matchsticks // 4
+
+        sticks = total_sticks // 4
+        # it can also only be 4 so top bottom right left
+        sides = [0] * 4
+        matchsticks.sort(reverse=True)
 
         def backtrack(i):
-            # basecasee
             if i == len(matchsticks):
                 return True
 
             for j in range(4):
-                if sides[j] + matchsticks[i] <= length:
+                if sides[j] + matchsticks[i] <= sticks:
+                    # move on also
                     sides[j] += matchsticks[i]
                     if backtrack(i + 1):
                         return True
