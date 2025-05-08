de3eddc6bc3d5b6557cdf32fbd504c793af18bb4
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-05-08 01:04:45 -0500

    Update 88_merge_sorted_array.py - LeetCode Problem 88

--- 88_merge_sorted_array.py
+++ 88_merge_sorted_array.py
@@ -3,19 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # since i know where the m and n will be and its sorted
         l = m - 1
         r = n - 1
         i = len(nums1) - 1
         while r >= 0 and l >= 0:
-            if nums1[l] < nums2[r]:
+            if nums1[l] <= nums2[r]:
                 nums1[i] = nums2[r]
                 r -= 1
             else:
                 nums1[i] = nums1[l]
                 l -= 1
             i -= 1
-        # this handles lets say every element is bigger than the r e.g [4,5,6] [1,2,3]
+
         while r >= 0:
             nums1[i] = nums2[r]
-            r -= 1
             i -= 1
+            r -= 1
