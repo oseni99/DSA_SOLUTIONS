a42460dbd8b1881fd943979878d2c421dad99f4b
Oluwatosin Oseni <143651626+oseni99@users.noreply.github.com>
2025-05-10 03:07:41 -0500

    Update 200_number_of_islands.py - LeetCode Problem 200

--- 200_number_of_islands.py
+++ 200_number_of_islands.py
@@ -1,35 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-
-        rows = len(grid)
-        cols = len(grid[0])
-        visit = set()
+        # i just want to get connected islands
+        rows, cols = len(grid), len(grid[0])
         islands = 0
+        visited = set()
 
-        def bfs(r, c):
-            queue = deque()
-            visit.add((r, c))
-            queue.append((r, c))
-            while queue:
-                row, col = queue.popleft()
-                directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-                for dr, dc in directions:
-                    nr = row + dr
-                    nc = col + dc
-                    if (
-                        nr in range(rows)
-                        and nc in range(cols)
-                        and grid[nr][nc] == "1"
-                        and (nr, nc) not in visit
-                    ):
-                        queue.append((nr, nc))
-                        visit.add((nr, nc))
+        def dfs(r, c):
+            # when i see like one i can move 4 times
+            if (
+                min(r, c) < 0
+                or r >= rows
+                or c >= cols
+                or (r, c) in visited
+                or grid[r][c] == "0"
+            ):
+                return
+            visited.add((r, c))
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visit:
-                    bfs(r, c)
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    # i should only check when i see one
+                    dfs(r, c)
                     islands += 1
         return islands
